22a7bccfea8073036557c08354f1978ede0d3164
Manuel Stingl <manuel@stingl.st>
2019-12-11 22:31:25 +0100

    AuthConsumer as standalone model - Requires a User referenced (the user can have a unusable password) - the key will be stored hashed using djangos hashers

--- models/auth.py
+++ models/auth.py
@@ -4,11 +4,23 @@ This module contains Database Models which persistently store data used for auth
 
 import uuid
 from django.db import models
-from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password, check_password
 
 
-UserModel = get_user_model()
-
-
-class AuthConsumer(UserModel):
+class AuthConsumer(models.Model):
     uid = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    key = models.CharField(max_length=128)
+    user = models.OneToOneField('User', on_delete=models.CASCADE, related_name='consumer')
+
+    @classmethod
+    def key_hash(cls, key):
+        """
+        Hashes a key with the django hashing method and returns it
+        """
+        return make_password(key)
+
+    def check_key(self, key):
+        """
+        Checks if a given key matches the stored key in the database.
+        """
+        return check_password(key, self.key)
